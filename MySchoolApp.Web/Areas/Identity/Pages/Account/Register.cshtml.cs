ed2ad882975b393d46e1a33e889442c117aeddc2
Karlus Pulley <KarlusTPulley@gmail.com>
2026-04-14 13:44:22 -0600

    add first name and last name when you register

--- MySchoolApp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MySchoolApp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -97,6 +97,23 @@ namespace MySchoolApp.Web.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {1} characters long.", 
+                MinimumLength = 2)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {1} characters long.", 
+                MinimumLength = 3)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            [Display(Name = "Date of Birth")]
+            public DateOnly DateOfBirth { get; set; }
         }
 
 
@@ -116,6 +133,11 @@ namespace MySchoolApp.Web.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                user.DateOfBirth = Input.DateOfBirth;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
